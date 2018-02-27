781e47340430c142ec6348156ecb7c2190eb6ea3
uros96 <34546412+uros96@users.noreply.github.com>
2018-02-27 19:44:02 +0100

    reprezentativno

--- projects/1_basic/src/rtl/clk_counter.vhd
+++ projects/1_basic/src/rtl/clk_counter.vhd
@@ -15,17 +15,17 @@ USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 ENTITY clk_counter IS 
-			GENERIC(
-				-- maksimalna vrednost broja do kojeg brojac broji
-				max_cnt : STD_LOGIC_VECTOR(25 DOWNTO 0) := "10111110101111000010000000" -- 50 000 000
-			);
-			PORT   (
-				clk_i     : IN  STD_LOGIC; -- ulazni takt
-				rst_i     : IN  STD_LOGIC; -- reset signal
-				cnt_en_i  : IN  STD_LOGIC; -- signal dozvole brojanja
-				cnt_rst_i : IN  STD_LOGIC; -- signal resetovanja brojaca (clear signal)
-				one_sec_o : OUT STD_LOGIC  -- izlaz koji predstavlja proteklu jednu sekundu vremena
-			);
+	GENERIC(
+		-- maksimalna vrednost broja do kojeg brojac broji
+		max_cnt : STD_LOGIC_VECTOR(25 DOWNTO 0) := "10111110101111000010000000" -- 50 000 000
+	);
+	PORT   (
+		clk_i     : IN  STD_LOGIC; -- ulazni takt
+		rst_i     : IN  STD_LOGIC; -- reset signal
+		cnt_en_i  : IN  STD_LOGIC; -- signal dozvole brojanja
+		cnt_rst_i : IN  STD_LOGIC; -- signal resetovanja brojaca (clear signal)
+		one_sec_o : OUT STD_LOGIC  -- izlaz koji predstavlja proteklu jednu sekundu vremena
+	);
 END clk_counter;
 
 ARCHITECTURE rtl OF clk_counter IS
